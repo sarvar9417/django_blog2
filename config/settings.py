bf69fa028aafd5af8ce70468de03d7002e65a901
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2021-07-23 10:24:53 +0500

    heroku-procfile

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lqv^zta+%s)@02a$sx1fe_i)^apkmj$jo8e7#jo)dt6=3p^j_3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
 
 
 # Application definition
